c2c6353a21d17fff17e0c8cc65c80c96e46163b2
Jasmine Qin <yijia.qin@alumni.ubc.ca>
2020-06-10 20:14:56 -0700

    add aggregated data for licence viz

--- src/04_visualization/licence_vis_synthesis.py
+++ src/04_visualization/licence_vis_synthesis.py
@@ -11,6 +11,8 @@ Usage: src/04_visualization/licence_vis_synthesis.py
 # load packages
 import pandas as pd
 import json
+import csv
+import re
 
 
 def main():
@@ -66,6 +68,68 @@ def main():
 
     # save to files
     licence_df.to_csv("data/processed/vis_licence.csv", index=False)
+    parking.to_csv("data/processed/vis_parking.csv", index=False)
+    disability_parking.to_csv(
+        "data/processed/vis_disability_parking.csv", index=False)
+
+    #################
+    # Aggregated df #
+    #################
+
+    df = pd.read_csv(
+        "data/processed/combined_licences.csv",
+        low_memory=False)
+
+    # organize FOLDERYEAR
+    df = df.loc[df.FOLDERYEAR.notnull()]
+    df['FOLDERYEAR'] = [y + 2000 if y >= 0 and y <
+                        90 else y + 1900 for y in df.FOLDERYEAR]
+    df['ExtractDate'] = pd.to_datetime(df['ExtractDate'], errors='ignore')
+    df['IssuedDate'] = pd.to_datetime(df['IssuedDate'], errors='ignore')
+    df['ExpiredDate'] = pd.to_datetime(df['ExpiredDate'], errors='ignore')
+
+    df.loc[(df['FOLDERYEAR']
+            < 1997.0) & (df['IssuedDate'].dt.year == 1996.0)
+           & (df['ExpiredDate'].dt.year == 1997.0), 'FOLDERYEAR'] = 1997.0
+
+    df = df[~(df.FOLDERYEAR < 1997.0)]
+    df['FOLDERYEAR'] = [int(i) for i in df['FOLDERYEAR']]
+
+    # only Issued licences
+    df = df.query('Status == "Issued"')
+
+    mapping_dict = {' ': ' '}
+
+    # Read csv and write to dictionary
+    with open("src/02_clean_wrangle/business_mapping_dictionary.csv",
+              mode='r') as csv_file:
+
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            mapping_dict[row[0]] = row[1]
+
+    # Remove additional key value pair
+    mapping_dict.pop(' ')
+    mapping_dict.pop('BusinessType')
+
+    # Add BusinessIndustry column
+    df['BusinessIndustry'] = df['BusinessType'].map(mapping_dict)
+
+    # Remove 2010 Winter games : Outlier
+    df = df[df.BusinessIndustry != 'Historic']
+    df = df[df.BusinessIndustry != 'Real estate and rental and leasing']
+
+    agg_viz = pd.DataFrame(df.groupby([
+        'FOLDERYEAR', 'LocalArea',
+        'BusinessIndustry', 'BusinessType'])[
+        'BusinessName'].count()).reset_index()
+
+    agg_viz = agg_viz[~(agg_viz.BusinessType.str.contains(
+        r'\*Historic\*'))]
+
+    # save to files
+    licence_df.to_csv("data/processed/vis_licence.csv", index=False)
+    agg_viz.to_csv("data/processed/vis_agg_licence.csv", index=False)
     parking.to_csv("data/processed/vis_parking.csv", index=False)
     disability_parking.to_csv(
         "data/processed/vis_disability_parking.csv", index=False)
